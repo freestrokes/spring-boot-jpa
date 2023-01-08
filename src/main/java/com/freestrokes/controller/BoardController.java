66116c7f0b9f27c4b62564955a2873fbcb3f6a24
freestrokes <freestrokes.hk@gmail.com>
2023-01-08 23:55:58 +0900

    [spring-boot-jpa/main] BoardController.java 수정

--- src/main/java/com/freestrokes/controller/BoardController.java
+++ src/main/java/com/freestrokes/controller/BoardController.java
@@ -15,15 +15,23 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class BoardController {
 
-    // TODO
+    // TODO: 테스트를 위해 추가
     private final ApplicationProperties applicationProperties;
 
     private final BoardService boardService;
 
+    // TODO: DI(Dependency Injection)에 대한 설명
+    // 아래 코드처럼 생성자를 통해 의존성 주입을 설정해주는데 이는 lombok의 @RequiredArgsConstructor를 사용해서 생략 가능.
+    // 스프링 IoC Container가 해당 의존성 타입에 맞는 bean을 만들어서 주입 (의존성 관리)
+//    public BoardController(ApplicationProperties applicationProperties, BoardService boardService) {
+//        this.applicationProperties = applicationProperties;
+//        this.boardService = boardService;
+//    }
+
     @GetMapping(path = "/boards", produces = "application/json")
     public ResponseEntity<List<BoardDto.ResponseDto>> getBoards() throws Exception {
 
-        // TODO
+        // TODO: 테스트를 위해 추가
         System.out.println("applicationProperties: " + applicationProperties);
 
         List<BoardDto.ResponseDto> result = boardService.getBoards();
