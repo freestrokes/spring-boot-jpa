2c9312aa9e5368456280899b7dd22530a50a27cc
freestrokes <freestrokes.hk@gmail.com>
2023-01-29 17:18:26 +0900

    [spring-boot-jpa/main] BoardController.java 주석 추가

--- src/main/java/com/freestrokes/controller/BoardController.java
+++ src/main/java/com/freestrokes/controller/BoardController.java
@@ -28,11 +28,19 @@ public class BoardController {
     // 레퍼런스에서는 생성자를 이용한 방법을 권장 (생성자는 lombok의 @RequiredArgsConstructor를 사용해서 생략 가능)
     // 필드, setter를 사용한 경우엔 의존성 주입 없이 인스턴스 생성이 가능하다는 문제가 있고
     // 생성자를 사용한 경우엔 순환 참조가 발생할 수도 있음.
+
+    // 생성자를 이용한 의존성 주입 예시.
 //    public BoardController(ApplicationProperties applicationProperties, BoardService boardService) {
 //        this.applicationProperties = applicationProperties;
 //        this.boardService = boardService;
 //    }
 
+    /**
+     * 게시글 목록 조회
+     *
+     * @return
+     * @throws Exception
+     */
     @GetMapping(path = "/boards", produces = "application/json")
     @LogExecutionTime
     public ResponseEntity<List<BoardDto.ResponseDto>> getBoards() throws Exception {
@@ -44,20 +52,42 @@ public class BoardController {
         return new ResponseEntity<List<BoardDto.ResponseDto>>(result, HttpStatus.OK);
     }
 
+    /**
+     * 게시글 등록
+     *
+     * @param boardRequestDto
+     * @return
+     * @throws Exception
+     */
     @PostMapping(path = "/boards", produces = "application/json")
     public ResponseEntity<BoardDto.ResponseDto> postBoard(@RequestBody BoardDto.RequestDto boardRequestDto) throws Exception {
         BoardDto.ResponseDto result = boardService.postBoard(boardRequestDto);
         return new ResponseEntity<BoardDto.ResponseDto>(result, HttpStatus.OK);
     }
 
+    /**
+     * 게시글 수정
+     *
+     * @param id
+     * @param boardRequestDto
+     * @return
+     * @throws Exception
+     */
     @PutMapping(path = "/boards/{id}", produces = "application/json")
-    public ResponseEntity<BoardDto.ResponseDto> putBoard(@PathVariable Long id, @RequestBody BoardDto.RequestDto boardRequestDto) throws Exception {
+    public ResponseEntity<BoardDto.ResponseDto> putBoard(@PathVariable String id, @RequestBody BoardDto.RequestDto boardRequestDto) throws Exception {
         BoardDto.ResponseDto result = boardService.putBoard(id, boardRequestDto);
         return new ResponseEntity<BoardDto.ResponseDto>(result, HttpStatus.OK);
     }
 
+    /**
+     * 게시글 삭제
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
     @DeleteMapping(path = "/boards/{id}", produces = "application/json")
-    public ResponseEntity<?> deleteBoard(@PathVariable Long id) throws Exception {
+    public ResponseEntity<?> deleteBoard(@PathVariable String id) throws Exception {
         boardService.deleteBoard(id);
         return new ResponseEntity<>("{}", HttpStatus.OK);
     }
