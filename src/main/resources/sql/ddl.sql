3f3cf1646a21ea8771ab34daf8a39f1a9367231b
freestrokes <freestrokes.hk@gmail.com>
2023-01-29 17:17:18 +0900

    [spring-boot-jpa/main] data.sql, ddl.sql 수정

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS board;
 DROP TABLE IF EXISTS board_comment;
 
 CREATE TABLE board (
-    board_id bigint(20) NOT NULL AUTO_INCREMENT COMMENT '게시판 id',
+    board_id varchar(100) NOT NULL COMMENT '게시글 id',
     title varchar(500) DEFAULT NULL COMMENT '제목',
     content text DEFAULT NULL COMMENT '내용',
     author varchar(100) DEFAULT NULL COMMENT '작성자',
@@ -11,8 +11,8 @@ CREATE TABLE board (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='게시판';
 
 CREATE TABLE board_comment (
-    board_comment_id bigint(20) NOT NULL AUTO_INCREMENT COMMENT '게시판 댓글 id',
-    board_id bigint(20) NOT NULL COMMENT '게시판 id',
+    board_comment_id varchar(100) NOT NULL COMMENT '게시글 댓글 id',
+    board_id varchar(100) NOT NULL COMMENT '게시글 id',
     content text DEFAULT NULL COMMENT '내용',
     author varchar(100) DEFAULT NULL COMMENT '작성자',
     PRIMARY KEY (board_comment_id),
