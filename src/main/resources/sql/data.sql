ce71be8ebbc5612019944f263ca9ea5bfbf6709b
freestrokes <freestrokes.hk@gmail.com>
2023-02-01 12:58:57 +0900

    [spring-boot-jpa/main] data.sql, ddl.sql 수정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -12,11 +12,11 @@ INSERT INTO
     testdb.board_comment(board_comment_id, board_id, content, author)
 VALUES
     ('141feac0-3da0-4a42-8271-038ead97280d', 'c41feac0-3da0-4a42-8271-038ead97280d', 'board1 comment content1', 'author1'),
-    ('241feac0-3da0-4a42-8271-038ead97280d', 'c41feac0-3da0-4a42-8271-038ead97280d', 'board1 comment content2', 'author1'),
-    ('341feac0-3da0-4a42-8271-038ead97280d', 'c41feac0-3da0-4a42-8271-038ead97280d', 'board1 comment content3', 'author1'),
-    ('4e0eca6b-0544-45ae-a2b9-7199126c21b5', 'ce0eca6b-0544-45ae-a2b9-7199126c21b5', 'board2 comment content1', 'author2'),
+    ('241feac0-3da0-4a42-8271-038ead97280d', 'c41feac0-3da0-4a42-8271-038ead97280d', 'board1 comment content2', 'author2'),
+    ('341feac0-3da0-4a42-8271-038ead97280d', 'c41feac0-3da0-4a42-8271-038ead97280d', 'board1 comment content3', 'author3'),
+    ('4e0eca6b-0544-45ae-a2b9-7199126c21b5', 'ce0eca6b-0544-45ae-a2b9-7199126c21b5', 'board2 comment content1', 'author1'),
     ('5e0eca6b-0544-45ae-a2b9-7199126c21b5', 'ce0eca6b-0544-45ae-a2b9-7199126c21b5', 'board2 comment content2', 'author2'),
-    ('6e0eca6b-0544-45ae-a2b9-7199126c21b5', 'ce0eca6b-0544-45ae-a2b9-7199126c21b5', 'board2 comment content3', 'author2'),
-    ('7bfe97a1-c29f-4758-abe9-9c8302b74d23', '8bfe97a1-c29f-4758-abe9-9c8302b74d23', 'board3 comment content1', 'author3'),
-    ('8bfe97a1-c29f-4758-abe9-9c8302b74d23', '8bfe97a1-c29f-4758-abe9-9c8302b74d23', 'board3 comment content2', 'author3'),
+    ('6e0eca6b-0544-45ae-a2b9-7199126c21b5', 'ce0eca6b-0544-45ae-a2b9-7199126c21b5', 'board2 comment content3', 'author3'),
+    ('7bfe97a1-c29f-4758-abe9-9c8302b74d23', '8bfe97a1-c29f-4758-abe9-9c8302b74d23', 'board3 comment content1', 'author1'),
+    ('8bfe97a1-c29f-4758-abe9-9c8302b74d23', '8bfe97a1-c29f-4758-abe9-9c8302b74d23', 'board3 comment content2', 'author2'),
     ('9bfe97a1-c29f-4758-abe9-9c8302b74d23', '8bfe97a1-c29f-4758-abe9-9c8302b74d23', 'board3 comment content3', 'author3')
--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -8,7 +8,7 @@ CREATE TABLE board (
     content text DEFAULT NULL COMMENT '내용',
     author varchar(100) DEFAULT NULL COMMENT '작성자',
     PRIMARY KEY (board_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='게시판';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='게시글';
 
 CREATE TABLE board_comment (
     board_comment_id varchar(100) NOT NULL COMMENT '게시글 댓글 id',
@@ -17,4 +17,4 @@ CREATE TABLE board_comment (
     author varchar(100) DEFAULT NULL COMMENT '작성자',
     PRIMARY KEY (board_comment_id),
     CONSTRAINT board_comment_fk1 FOREIGN KEY (board_id) REFERENCES board (board_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='게시판 댓글';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='게시글 댓글';
