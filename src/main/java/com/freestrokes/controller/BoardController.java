c6e306880ad1b57aec3d584c27c28a4c35ef8650
freestrokes <freestrokes.hk@gmail.com>
2023-02-02 10:01:02 +0900

    [spring-boot-jpa/main] 주석 수정

--- src/main/java/com/freestrokes/controller/BoardController.java
+++ src/main/java/com/freestrokes/controller/BoardController.java
@@ -16,18 +16,18 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class BoardController {
 
-    // TODO: 테스트를 위해 추가
+    // TODO: ApplicationProperties 테스트를 위해 추가
     private final ApplicationProperties applicationProperties;
 
     private final BoardService boardService;
 
     // TODO: DI(Dependency Injection)에 대한 설명
-    // 일반적으로 아래 코드처럼 생성자를 통해 의존성 주입을 설정해주는데 이는 lombok의 @RequiredArgsConstructor를 사용해서 생략 가능.
+    // 일반적으로 아래 코드처럼 생성자를 통해 의존성 주입을 설정해줌.
     // 스프링 IoC Container가 해당 의존성 타입에 맞는 bean을 만들어서 주입 (의존성 관리)
-    // 의존성 주입에는 생성자, 필드, setter 3가지 방법이 있는데
+    // 의존성 주입에는 생성자, 필드 + setter 2가지 방법이 있는데
     // 레퍼런스에서는 생성자를 이용한 방법을 권장 (생성자는 lombok의 @RequiredArgsConstructor를 사용해서 생략 가능)
-    // 필드, setter를 사용한 경우엔 의존성 주입 없이 인스턴스 생성이 가능하다는 문제가 있고
-    // 생성자를 사용한 경우엔 순환 참조가 발생할 수도 있음.
+    // 필드 + setter를 사용한 경우엔 의존성 주입 없이 인스턴스 생성이 가능하다는 문제가 있음. (이 경우엔 @Autowired 어노테이션을 사용해야 함)
+    // 생성자를 사용한 경우엔 순환 참조가 발생할 수 있음.
 
     // TODO: 생성자를 이용한 의존성 주입(DI) 예시.
 //    public BoardController(ApplicationProperties applicationProperties, BoardService boardService) {
@@ -45,7 +45,7 @@ public class BoardController {
     @LogExecutionTime
     public ResponseEntity<List<BoardDto.ResponseDto>> getBoards() throws Exception {
 
-        // TODO: 테스트를 위해 추가
+        // TODO: ApplicationProperties 테스트를 위해 추가
         System.out.println("applicationProperties: " + applicationProperties);
 
         List<BoardDto.ResponseDto> result = boardService.getBoards();
