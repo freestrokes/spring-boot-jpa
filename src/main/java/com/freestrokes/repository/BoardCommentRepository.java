aa61bdcf7ae4eafac84466e581e359b0a4621e8a
freestrokes <freestrokes.hk@gmail.com>
2023-02-12 17:19:33 +0900

    [spring-boot-jpa] fix: BoardCommentRepository.java 수정 (@EntityGraph 추가)

--- src/main/java/com/freestrokes/repository/BoardCommentRepository.java
+++ src/main/java/com/freestrokes/repository/BoardCommentRepository.java
@@ -1,9 +1,20 @@
 package com.freestrokes.repository;
 
 import com.freestrokes.domain.BoardComment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BoardCommentRepository extends JpaRepository<BoardComment, String> {
+
+    // TODO: CASE2) repository 메서드에 @EntityGraph 사용하여 연관 객체 조회
+//    @EntityGraph(
+//        attributePaths = {"board"},
+//        type = EntityGraph.EntityGraphType.FETCH
+//    )
+//    Optional<BoardComment> findByBoardCommentId(String boardCommentId);
+
 }
--- src/main/java/com/freestrokes/service/BoardCommentService.java
+++ src/main/java/com/freestrokes/service/BoardCommentService.java
@@ -64,6 +64,9 @@ public class BoardCommentService {
         // 게시글 댓글 조회
         BoardComment findBoardComment = boardCommentRepository.findById(boardCommentId).orElseThrow(NoSuchElementException::new);
 
+        // TODO: CASE2) repository 메서드에 @EntityGraph 사용하여 연관 객체 조회
+//        BoardComment findBoardComment = boardCommentRepository.findByBoardCommentId(boardCommentId).orElseThrow(NoSuchElementException::new);
+
         // 게시글 댓글 저장
         findBoardComment.updateBoardComment(
             boardCommentRequestDto.getContent(),
@@ -74,9 +77,12 @@ public class BoardCommentService {
         // 연관관계의 FetchType LAZY로 설정된 객체는 조회시 바로 초기화되지 않고 proxy 객체로 처리 됨.
         // 이 상태에서 proxy 객체를 타입 캐스팅하려는 경우 could not initialize proxy - no session 에러가 발생하게 됨.
         // FetchType EAGER로 설정하면 해결되지만 N+1 문제가 발생할 수 있기 때문에 권장되지 않음.
-        // 따라서 아래와 같이 proxy 객체의 값을 꺼내와서 DTO에 담아 반환하는 방법이 권장 됨.
+        // CASE1) repository 메서드에 객체그래프 탐색을 위한 @EntityGraph를 사용
+        // CASE2) proxy 객체의 값을 꺼내와서 DTO에 담아 반환
+
         return BoardCommentDto.ResponseDto.builder()
             .boardCommentId(findBoardComment.getBoardCommentId())
+            // TODO: CASE1) proxy 객체의 값을 꺼내서 DTO에 담아 반환
             .board(
                 Board.builder()
                     .boardId(findBoardComment.getBoard().getBoardId())
@@ -85,6 +91,8 @@ public class BoardCommentService {
                     .author(findBoardComment.getBoard().getAuthor())
                     .build()
             )
+            // TODO: CASE2) repository 메서드에 @EntityGraph 사용하여 연관 객체 조회
+//            .board(findBoardComment.getBoard())
             .content(findBoardComment.getContent())
             .author(findBoardComment.getAuthor())
             .build();
