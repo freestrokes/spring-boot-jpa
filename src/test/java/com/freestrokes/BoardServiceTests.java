adf9ade181d0b5f2f1c5497d4f2eb9c7233f3e36
freestrokes <freestrokes.hk@gmail.com>
2023-10-15 18:45:52 +0900

    [main] feat: Board 서비스 테스트 수정

--- src/test/java/com/freestrokes/BoardServiceTests.java
+++ src/test/java/com/freestrokes/BoardServiceTests.java
@@ -6,22 +6,26 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RequiredArgsConstructor
 @RunWith(SpringRunner.class)
+@SpringBootTest()
 public class BoardServiceTests {
 
-//	private final BoardRepository boardRepository;
-//
-//	@Before
-//	public void setup() {
-//	}
-//
-//	@After
-//	public void cleanAll() {
-//		boardRepository.deleteAll();
-//	}
+	@Autowired
+	private BoardRepository boardRepository;
+
+	@Before
+	public void setup() {
+	}
+
+	@After
+	public void cleanAll() {
+		boardRepository.deleteAll();
+	}
 
 	@Test
 	public void contextLoads() {
