2608fbccdee3fa46977e4ee4a86b6036682a7f3d
freestrokes <freestrokes.hk@gmail.com>
2023-10-20 20:34:42 +0900

    [main] feat: BoardServiceTests 수정

--- src/test/java/com/freestrokes/BoardServiceTests.java
+++ src/test/java/com/freestrokes/BoardServiceTests.java
@@ -1,5 +1,8 @@
 package com.freestrokes;
 
+import com.freestrokes.domain.Board;
+import com.freestrokes.domain.BoardComment;
+import com.freestrokes.repository.BoardCommentRepository;
 import com.freestrokes.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.junit.After;
@@ -18,17 +21,48 @@ public class BoardServiceTests {
 	@Autowired
 	private BoardRepository boardRepository;
 
+	@Autowired
+	private BoardCommentRepository boardCommentRepository;
+
 	@Before
 	public void setup() {
+		// 게시글 생성
+		Board board = Board.builder()
+			.title("board title test")
+			.content("board content test")
+			.author("board author test")
+			.build();
+
+		// 게시글 저장
+		boardRepository.save(board);
+
+		for (int i = 0; i < 10; i++) {
+			// 게시글 댓글 생성
+			BoardComment boardComment = BoardComment.builder()
+				.board(board)
+				.content("board comment test " + i)
+				.author("board comment test " + i)
+				.build();
+
+			boardCommentRepository.save(boardComment);
+		}
+
+//		entityManager.flush();
+//		entityManager.clear();
 	}
 
 	@After
 	public void cleanAll() {
-		boardRepository.deleteAll();
+//		boardCommentRepository.deleteAll();
+//		boardRepository.deleteAll();
 	}
 
 	@Test
 	public void contextLoads() {
+		//given
+//		List<String> subjectNames = academyService.findAllSubjectNames();
+		//then
+//		assertThat(subjectNames.size(), is(10));
 	}
 
 }
