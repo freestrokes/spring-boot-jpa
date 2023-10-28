1c478888200529f30a8f2788e632831cd162e429
freestrokes <freestrokes.hk@gmail.com>
2023-10-28 17:03:15 +0900

    [main] feat: BoardRepository 수정

--- src/main/java/com/freestrokes/repository/BoardRepository.java
+++ src/main/java/com/freestrokes/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package com.freestrokes.repository;
 
 import com.freestrokes.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, String> {
+
+    Page<Board> findAll(Pageable pageable);
+
 }
